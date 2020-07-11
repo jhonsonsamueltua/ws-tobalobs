4fe67dcbb5c42f17f3d392be4479122d0f06f75b
Jhonson Samuel Tua Hutagaol <jhonsonhutagaol99@gmail.com>
2020-07-11 21:13:45 +0700

    test remote

--- script/script.sh
+++ script/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh pi@10.42.0.230 ./sketchbook/tobalobs/script-remote.sh $1 &
+ssh -l pi proxy73.rt3.io -p 35745 ./sketchbook/tobalobs/script-remote.sh $1 &
 # ssh pi@10.42.0.230 ls
 # cd ..
 # cd Documents/tobalobs/
